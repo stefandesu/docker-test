af95c16f05335c6c5090f6bc00e402b76ee60181
Stefan Peters <stefandesu@exo.pm>
2020-05-14 14:59:41 +0200

    Adjust index

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World 1.0.4');
+  res.send('Hello World Version ' + require("./package.json").version);
 });
 
 app.listen(PORT, HOST);
